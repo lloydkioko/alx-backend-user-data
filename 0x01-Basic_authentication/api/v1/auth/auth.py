4ccc4c99a456c6ba14114847f851551df5876311
lloyd kioko <ch.abene254@gmail.com>
2023-10-31 16:40:48 +0300

    fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,42 +1,69 @@
 #!/usr/bin/env python3
-"""
-API Authentication Module
-"""
-from flask import request
-import re
+
 from typing import List, TypeVar
+from flask import Flask, request
 
 
 class Auth:
-    """
-    API Authentication Class
-    """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Returns True if the path is not in excluded_paths,
-        thus requires authentication
-        """
-        if (not path or not excluded_paths):
+    ''' A Class to manage the API authentication.
+    '''
+
+    def require_auth(
+            self,
+            path: str,
+            excluded_paths: List[str]
+            ) -> bool:
+        ''' Required auth
+        '''
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        path = path + '/' if not path.endswith('/') else path
-        for ex_path in excluded_paths:
-            match = re.match(ex_path + "([*]?|(/)?)", path)
-            if match:
-                return False
+        # handle * at end of excluded paths
+        if path[-1] == '/':
+            path = path[:-1]
+
+        contains_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                contains_slash = True
+
+            if excluded_path.endswith('*'):
+                idx_after_last_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[idx_after_last_slash:-1]
+
+                idx_after_last_slash = path.rfind('/') + 1
+                tmp_path = path[idx_after_last_slash:]
+
+                if excluded in tmp_path:
+                    return False
+
+            if contains_slash:
+                contains_slash = False
+
+        path += '/'
+
+        if path in excluded_paths:
+            return False
+
         return True
 
-    def authorization_header(self, request=None) -> str:
-        """
-        Validates all requests to secure the API
-        """
-        if request is None or 'Authorization' not in request.headers:
+    def authorization_header(
+            self,
+            request=None
+            ) -> str:
+        '''Auth header
+        '''
+        if request is None:
             return None
 
         return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Returns None
-        """
+    def current_user(
+            self,
+            request=None
+            ) -> TypeVar('User'):
+        ''' Current User
+        '''
+        request = Flask(__name__)
         return None
